292b678d05272aa7e9da910e8abe1fecc7f57992
Harlequelrah <degbovimax19@gmail.com>
2025-03-31 23:50:48 +0000

    installation de breeze

--- routes/web.php
+++ routes/web.php
@@ -1,54 +1,20 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\PlotController;
-use App\Http\Controllers\InterventionController;
-use App\Http\Controllers\TestController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', [AuthController::class, 'login'])->name('auth.login');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/admin-test', function () {
-    return "Welcome to the admin";
-})->middleware('role:Administrateur');
-
-Route::get('/admin', [TestController::class, 'index']);
-
-Route::prefix('/plot')->controller(PlotController::class)->name('plot.')->group(function () {
-    Route::get('/plot', 'index')->name('index');
-
-    Route::get("/{plot}/edit", 'edit')->name('edit');
-
-    Route::plot("/{plot}/edit", 'update');
-
-    Route::plot("/new", 'store');
-
-    Route::get("/new", 'create')->name('create');
-
-Route::get("/{plot}", 'show')
-    ->where([
-        'plot' => '[0-9]+']
-    )->name('show');
-
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::prefix('/intervention')->controller(InterventionController::class)->name('intervention.')->group(function () {
-    Route::get('/intervention', 'index')->name('index');
 
-    Route::get("/{intervention}/edit", 'edit')->name('edit');
-
-    Route::intervention("/{intervention}/edit", 'update');
-
-    Route::intervention("/new", 'store');
-
-    Route::get("/new", 'create')->name('create');
-
-    Route::get("/{intervention}", 'show')
-        ->where([
-            'intervention' => '[0-9]+']
-        )->name('show');
-
-});
+require __DIR__.'/auth.php';
