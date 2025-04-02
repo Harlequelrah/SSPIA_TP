8912dae342981f4109923200ce66c3f2b3ad02fa
uche2064 <godswilllek02@gmail.com>
2025-04-02 08:08:13 +0000

    ajout du bouton retour ainsi que du composant(formulaire) d'ajout de parcelles

--- resources/views/components/plots.blade.php
+++ resources/views/components/plots.blade.php
@@ -1,19 +1,20 @@
-<div>
-    <div class="flex items-center justify-between mb-6">
-        <x-heading title="Gestion des parcelles" />
+<div class="mt-4 px-4">
+    <x-back-to-home />
 
-        <button @click="" class="bg-[#4a7c59] text-white px-3 py-2 rounded-lg cursor-pointer">
-            <i class="fa-solid fa-plus"></i>
-            <span>Ajouter une parcelle</span>
-        </button>
-    </div>
-    <!-- liste des plots -->
-    <div class="flex items-center flex-wrap">
-        <x-info-plot
-            status='En culture' 
-            size='2 hectares'
-            crop='Blé'
-            plantationDate='2024-12-26'
-            />
+    <div class="flex flex-col md:flex-col lg:flex-row lg:items-start lg:justify-around gap-6">
+        <!-- Liste des parcelles -->
+        <h1 class="font-bold text-lg md:block lg:hidden">Liste des parcelles</h1>
+        <div class="flex md:overflow-x-auto md:space-x-4 lg:flex-col lg:space-y-4 lg:overflow-y-auto lg:h-[730px] sm:w-[700px] lg:w-[400px] sm:overflow-x-auto sm:space-x-4">
+            <h1 class="font-bold mb-4 text-lg hidden md:hidden lg:block">Liste des parcelles</h1>
+
+            <x-info-plot status="En culture" size="2 hectares" crop="Blé" plantationDate="2024-12-26" />
+            <x-info-plot status="En culture" size="2 hectares" crop="Blé" plantationDate="2024-12-26" />
+            <x-info-plot status="En culture" size="2 hectares" crop="Blé" plantationDate="2024-12-26" />
+            <x-info-plot status="En culture" size="2 hectares" crop="Blé" plantationDate="2024-12-26" />
+            <x-info-plot status="En culture" size="2 hectares" crop="Blé" plantationDate="2024-12-26" />
+        </div>
+
+        <!-- Formulaire d'ajout d'une parcelle -->
+        <x-form-plot class="w-full lg:w-full sm:max-w-3xl" />
     </div>
 </div>
