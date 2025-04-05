61ad78cece8aecdeec992cfc29e8074ee81ea8f0
uche2064 <godswilllek02@gmail.com>
2025-04-05 17:03:12 +0000

    suppressions du dossier public/storage de .gitignore

--- resources/views/components/dashboard.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="p-10">
-    <div class="grid grid-cols-3 gap-4">
-        <x-dashboard-card title="Agriculteurs" count="00" activeMenu="users" />
-        <x-dashboard-card title="Parcelles" count="00" activeMenu="landmark" />
-        <x-dashboard-card title="Interventions" count="00" activeMenu="bolt" />
-    </div>
-</div>
