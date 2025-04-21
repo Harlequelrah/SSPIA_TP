07b5edd6d1b2c50ee6d5cbcc1d5345829674f248
uche2064 <godswilllek02@gmail.com>
2025-04-21 14:02:02 +0000

    modification du composant de notification

--- resources/views/components/notification.blade.php
+++ resources/views/components/notification.blade.php
@@ -1,9 +1,22 @@
-@props(['message', 'color', 'icon'])
-
+@props(['message', 'color' => 'green', 'icon' => 'fa-circle-check'])
 
 <div
-    class="absolute text-md bg-{{ $color }}-100 bottom-0 right-0 text-{{ $color }}-900 p-3 rounded-lg mb-4">
-    <i class="fa-solid {{ $icon }} text-{{ $color }}-600 mr-2"></i>
+    x-data="{ show: true }"
+    x-init="setTimeout(() => show = false, 4000)"
+    x-show="show"
+    x-transition:enter="transition ease-out duration-300"
+    x-transition:enter-start="opacity-0 translate-y-2"
+    x-transition:enter-end="opacity-100 translate-y-0"
+    x-transition:leave="transition ease-in duration-200"
+    x-transition:leave-start="opacity-100 translate-y-0"
+    x-transition:leave-end="opacity-0 translate-y-2"
+    @class([
+        'fixed text-md p-3 rounded-lg bottom-4 right-4 z-50 shadow-lg',
+        "bg-{$color}-100" => $color,
+        "text-{$color}-900" => $color,
+    ])>
+
+    <i @class(["fa-solid", $icon, "text-{$color}-600", "mr-2"])></i>
     <span class="text-sm">
         {{ $message }}
     </span>
