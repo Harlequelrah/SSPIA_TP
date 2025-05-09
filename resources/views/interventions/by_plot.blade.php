d1b9ca46b9e741f0ae536183aedf74a4af0b9dc0
uche2064 <godswilllek02@gmail.com>
2025-05-09 09:13:38 +0000

    légère modification

--- resources/views/interventions/by_plot.blade.php
+++ resources/views/interventions/by_plot.blade.php
@@ -9,25 +9,27 @@
         <x-notification :message="session('error')" color="red" icon="fa-circle-exclamation" />
     @endif
     <div class="max-w-6xl mx-auto p-4" x-data="{ interventionIdToDelete: null }">
-        <h1 class="text-2xl font-bold mb-4 text-[#4a7c59]">
+        <h1 class="text-2xl font-bold mb-4 text-teal-600">
             Interventions de la parcelle : {{ $plot->name }}
         </h1>
 
         <div class="flex items-center justify-between">
             <a href="{{ route('plots.index') }}" class="mb-4 inline-block text-sm text-blue-600 hover:underline">
-                ← Retour à la liste des parcelles
+                <i class="fa-solid fa-arrow-left text-teal-600"></i>
             </a>
 
             <a href="{{ route('plot.etiquette', $plot->id) }}"
-                class="border text-green-800 border-green-800 rounded-lg px-3 py-2 text-sm hover:bg-green-800 hover:text-white transition-colors duration-200">
-                {{ __('Imprimer etiquette ') }}
+                class="inline-flex items-center px-4 py-3 bg-white border border-green-300 rounded-md font-semibold text-xs text-green-700 tracking-widest shadow-sm hover:bg-green-50 focus:outline-none disabled:opacity-25 cursor-pointer transition ease-in-out duration-150">
+                {{__('Imprimer étiquette')}}
             </a>
+
+
         </div>
 
         <div class="overflow-x-auto bg-white rounded-lg shadow mt-4">
             <table class="min-w-full bg-white">
                 <thead>
-                    <tr class="bg-[#4a7c59] text-white">
+                    <tr class="bg-teal-600 text-white">
                         <th class="py-2 px-4 text-left">ID</th>
                         <th class="py-2 px-4 text-left">Type</th>
                         <th class="py-2 px-4 text-left">Date</th>
@@ -42,13 +44,13 @@
                             <td class="py-2 px-4">{{ $intervention->intervention_date }}</td>
                             <td class="py-2 px-4 flex space-x-2">
                                 <a href="{{ route('interventions.show', $intervention->id) }}"
-                                    class="bg-blue-600 text-white px-3 py-2 rounded hover:bg-blue-800">
-                                    Voir
+                                    class=" px-3 py-2 rounded">
+                                    <i class="fa-solid fa-eye text-blue-600 text-lg"></i>
                                 </a>
                                 <a href="#"
                                     @click.prevent="interventionIdToDelete = '{{ $intervention->id }}'; $dispatch('open-modal', 'confirm-delete')"
-                                    class="px-3 py-2 bg-red-600 cursor-pointer rounded-md hover:bg-red-800">
-                                    <i class="fa-solid fa-trash-alt text-white"></i>
+                                    class="px-3 py-2 cursor-pointer rounded-md">
+                                    <i class="fa-solid fa-trash-alt text-lg text-red-600"></i>
                                 </a>
                             </td>
                         </tr>
